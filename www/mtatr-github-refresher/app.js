ef6cfa17b059bd8bf42caed6c5a5bdb5c70e861e
umithyo <umit.haciyusufoglu@gmail.com>
2021-06-22 23:27:07 +0300

    chore(devops): added github refresher

---
+++ www/mtatr-github-refresher/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const port = 8201;
+const MTAClient = require('mtasa').Client;
+const mta = new MTAClient(process.env.MTA_HOST, process.env.MTA_PORT, process.env.MTA_HTTP_USER, process.env.MTA_HTTP_PASSWORD);
+
+app.get('/', (req, res) => {
+    res.send('App works.');
+});
+
+app.get('/trigger-refreshall', async (req, res) => {
+    const result = await mta.resources.github_refresh_receiver.triggerRefreshAll();
+    if (result) {
+        res.sendStatus(200);
+    } else {
+        res.sendStatus(400);
+    }
+});
+
+app.listen(port, () => {
+    console.log(`[GITHUB-REFRESHER] listening at http://localhost:${port}`);
+})
