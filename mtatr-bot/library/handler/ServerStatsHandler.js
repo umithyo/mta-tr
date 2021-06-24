cfcbfa16a116e74570480d89afabb02b59e1a7fa
umithyo <umit.haciyusufoglu@gmail.com>
2021-06-24 18:29:28 +0300

    fix(discord): echo channel topic removed alltogether

--- mtatr-bot/library/handler/ServerStatsHandler.js
+++ mtatr-bot/library/handler/ServerStatsHandler.js
@@ -11,33 +11,7 @@ class ServerStatsHandler extends Handler {
     }
 
     execute(bot, session, type, payload) {
-        if (type == "serverstats.countandname") {
-            if (payload.name == null) {
-                console.log(`serverstats.countandname cannot continue, 'payload.name' was not set ${payload.name}`);
-                return;
-            }
-            if (payload.count == null) {
-                console.log(`serverstats.countandname cannot continue, 'payload.count' was not set ${payload.count}`);
-                return;
-            }
-            if (payload.max_count == null) {
-                console.log(`serverstats.countandname cannot continue, 'payload.max_count' was not set ${payload.max_count}`);
-                return;
-            }
-
-            console.log(`Server stats:`);
-            console.log(JSON.stringify(payload));
-            let count = this.escape(payload.count);
-            let max_count = this.escape(payload.max_count);
-            let name = this.escape(payload.name);
-            name = name == 'MTA:SA' ? process.env.HOST_IP : name;
-            bot.setChannelStatus(name)
-                .then(updated => {
-                    console.log('updated channel topic:');
-                    console.log(`${updated}`);
-                })
-                .catch(() => console.error("bot.setChannelStatus error @ ServerStatsHandler.js#23"));
-        } else if (type == "serverstats.players") {
+        if (type == "serverstats.players") {
             if (!payload.players)
                 return;
             const playersToStr = payload.players.join('\n');
